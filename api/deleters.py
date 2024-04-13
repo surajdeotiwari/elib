bf56d973e6817ea2ea5469be76a72e33bf8bee92
surajdeotiwari <tiwarisurajdeo@gmail.com>
2024-04-13 18:55:51 +0530

    changes to getters and setters

--- api/deleters.py
+++ api/deleters.py
@@ -1,11 +1,25 @@
 from flask_restful import Resource
+from db.db import db, User, Author, Books
+from flask import make_response,request
 class DeleteBook(Resource):
     def get(self):
-        return "deletes the book"
+        book_id = request.form["id"]
+        book = db.get_or_404(Books,book_id)
+        db.session.delete(book)
+        db.session.commit()
+        return make_response("Book has been deleted",200)
 class DeleteUser(Resource):
     def get(self):
-        return "deletes the user"
+        user_name = request.form["username"]
+        user = db.get_or_404(User,user_name)
+        db.session.delete(user)
+        db.session.commit()
+        return make_response("User has been deleted",200)
 class DeleteAuthor(Resource):
     def get(self):
-        return "deletes the author"
+        author_id = request.form["id"]
+        author = db.get_or_404(Author,author_id)
+        db.session.delete(author)
+        db.session.commit()
+        return make_response("Success",200)
     
