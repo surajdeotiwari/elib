e250026acef0fb1c43cc6657b5830b4f7ed934d2
surajdeotiwari <tiwarisurajdeo@gmail.com>
2024-04-13 17:15:06 +0530

    changes to getters and setters

--- api/setters.py
+++ api/setters.py
@@ -1,11 +1,27 @@
 from flask_restful import Resource
-
+from flask import request, make_response
+from db.db import db, User, Books, Author
 class ChangeUserName(Resource):
-    def get(self):
-        return "this returns changed user name of a user"
+    def post(self):
+        id = request.form['id']
+        name = request.form['name']
+        user = db.get_or_404(User,id)
+        user.name = name
+        db.session.commit()
+        return make_response("Name name has been edited",200)
 class ChangeBookName(Resource):
-    def get(self):
-        return "changes the book name"
+    def post(self):
+        id = request.form['id']
+        name = request.form['name']
+        book = db.get_or_404(Books,id)
+        book.name = name
+        db.session.commit()
+        return make_response("Book name has been edited",200)
 class ChangeAuthorName(Resource):
-    def get(self):
-        return "changes the author name"
+    def post(self):
+        id = request.form['id']
+        name = request.form['name']
+        author = db.get_or_404(author,id)
+        author.name = name
+        db.session.commit()
+        return make_response("Author name has been edited",200)
--- server.sh
+++ server.sh
@@ -1,2 +1,4 @@
+# adding all libraries in requirement.txt
+pip freeze > requirement.txt
 # This is for server setup
 python3 app.py
